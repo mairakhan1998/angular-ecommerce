2acf8f8843c137c0c78092cb044655582e34c65a
mairakhan1998 <munshat19@gmail.com>
2024-05-27 12:22:55 +0530

    Angular project set up

---
+++ Downloads/frontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Product } from '../../../common/product';
+import { ProductService } from '../../../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+  products:Product[] =[];
+
+  constructor(private productService:ProductService){}
+
+  ngOnInit(): void{
+    this.listProducts();
+  }
+  listProducts() {
+    this.productService.getProductList().subscribe(data => {
+        this.products = data;
+      }
+    )
+  }
+
+}
---
+++ Downloads/frontEnd/angular-ecommerce/src/common/product.ts
@@ -0,0 +1,16 @@
+export class Product {
+    constructor(
+        public sku:string,
+        public name:string,
+        public description:string,
+        public unitPrice:string,
+        public imgUrl:string,
+        public active:string,
+        public unitsInStock:string,
+        public dateCreataed:string,
+        public lastUpdated:string,
+        
+    ){
+
+    }
+}
